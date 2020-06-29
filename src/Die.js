04ac5dcad65fe73743d33524119c5f3b6a923101
DaveFres <nanyandave@gmail.com>
2020-06-29 18:30:04 +0300

    fix(Die): remove re-creation of a onclick function

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor() {
+    super();
+    this.handleDiceClick = this.handleDiceClick.bind(this);
+  }
+
+  handleDiceClick() {
+    const {handleClick, idx} = this.props;
+    handleClick(idx);
+  }
+
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => this.props.handleClick(this.props.idx)}
+        onClick={this.handleDiceClick}
       >
         {this.props.val}
       </button>
